46816d36b2cc25a090c9ba42bce7309a30f36f33
nasarmuhammad1 <95246779+nasarmuhammad1@users.noreply.github.com>
2021-11-30 00:12:36 +0500

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://bison.studio</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://bison.studio/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://bison.studio/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://bison.studio/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://bison.studio/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://bison.studio/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
